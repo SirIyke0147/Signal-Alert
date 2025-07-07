16d8dd28b58288488c742fb4a5635d3411d44388
Ikenna Kalu Okorie <ikennaprim@gmail.com>
2025-07-07 20:41:13 +0100

    Update Forex_Signal_Bot.py

--- Forex_Signal_Bot.py
+++ Forex_Signal_Bot.py
@@ -1,5 +1,7 @@
-# forex_scanner.py
+# Add before other imports
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import requests
 import pandas as pd
 import time
